1526d323289fcde403d7837a0b7229a840f61c45
David <jobroman83@gmail.com>
2020-09-17 18:30:57 +0200

    t4 side bar completed and component journal main nothing

---
+++ src/components/journal/JournalEntry.js
@@ -0,0 +1,31 @@
+import React from 'react'
+
+export const JournalEntry = () => {
+    return (
+        <div className='journal__entry pointer'>
+            <div className='journal__entry-picture'
+            style={{
+                backgroundSize:'cover',
+                backgroundImage:'url(https://www.landuum.com/wp-content/uploads/2019/03/cultura_paisajeiluminado_landuum5.jpg)'
+            }}
+            >
+            </div>
+            <div className='journal__entry-body'>
+                <p className='journal__entry-title'>
+                    Un nuevo dia
+                </p>
+                <p className='journal__entry-content'>
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+                make a type specimen book.
+                </p>
+            </div>
+            <div className='journal__entry-date-box'>
+                <span>Monday</span>
+                <h4>28</h4>
+            </div>
+
+        </div>
+    )
+}
+
+export default JournalEntry
