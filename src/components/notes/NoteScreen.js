ce5758d833a6758b0a32a49ae215b599a0dcf33b
David <jobroman83@gmail.com>
2020-09-17 20:18:53 +0200

    t6 finishing interface of journalApp

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -7,7 +7,24 @@ export const NoteScreen = () => {
         <div className='notes__main-content'>
             
             <NotesAppBar/>
-        
+            <div className='notes__content'>
+                <input type='text'
+                        placeholder='Write some awesome'
+                        className='notes__title-input'
+                        autoComplete='off'
+                />
+                <textarea
+                placeholder='What happened today?'
+                className='notes__textarea'
+                >
+                </textarea>
+                <div className='notes__image'>
+                        <img
+                            src='https://cdn.pixabay.com/photo/2017/06/05/22/49/digital-marketing-2375583_960_720.jpg'
+                            alt='arbol'
+                        />
+                    </div>
+            </div>
         </div>
     )
 }
