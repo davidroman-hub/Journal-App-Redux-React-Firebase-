320aa6cf80f5dcd63773886ac42b36707d0529f8
David <jobroman83@gmail.com>
2020-09-17 21:40:56 +0200

    task 2 first dispatch to the state

--- src/components/auth/LoginScreen.js
+++ src/components/auth/LoginScreen.js
@@ -1,24 +1,50 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
+import { useForm } from '../../hooks/useForm'
+import { useDispatch } from 'react-redux'
 
+//Actions Redux
+
+import { login } from '../../actions/auth'
 
 export const LoginScreen = () => {
+
+    const dispatch = useDispatch();
+
+    const [ formValues, handleInputChange ] = useForm ({
+        email:'david@gmail.com',
+        password:'123456'
+    })
+
+    const {email,password} = formValues;
+
+    const handleLogin = (e) => {
+        e.preventDefault()
+        console.log(formValues)
+        dispatch(login(12345,'hernan'))
+    }
+
+
     return (
         <>
             <h3 className='auth__title'>Login</h3>
-            <form>
+            <form onSubmit={handleLogin}>
                 <input
                     type='email'
                     placeholder='E-mail'
                     name='email'
                     className='auth__input'
                     autoComplete='off'
+                    value={email}
+                    onChange={handleInputChange}
                 />
                 <input
                     type='password'
                     placeholder='Password'
                     name='password'
                     className='auth__input'
+                    value={password}
+                    onChange={handleInputChange}
                 />
                 <button  
                 className='btn btn-primary btn-block'
