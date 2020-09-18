c77d51f12081cc75cad6b7eb3075feabe302c009
David <jobroman83@gmail.com>
2020-09-18 15:15:39 +0200

    task 12 hold the auth with useEffect

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import {
     BrowserRouter as Router,
     Switch,
@@ -6,10 +6,31 @@ import {
     Redirect
     
 } from 'react-router-dom';
+import {useDispatch} from 'react-redux';
+
+
+import {firebase} from "../firebase/firebase-config";
 import  AuthRouter  from './AuthRouter';
 import JournalScreen from '../components/journal/JorunalScreen'
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+
+    const dispatch = useDispatch()
+    
+    useEffect(() => {
+        firebase.auth().onAuthStateChanged((user) => {
+
+            //console.log(user)
+            // if the object user have something 
+            if(user?.uid){
+                    // we exec login and bring the values also the email if your want
+                dispatch(login(user.uid, user.displayName))
+            }
+        })
+    }, [dispatch])
+    
+    
     return (
         <Router>
             <div>
