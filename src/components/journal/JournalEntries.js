3194634de0471cadeafad78ca1b5af447efafc57
David <jobroman83@gmail.com>
2020-09-29 17:51:44 +0200

    task 6 show notes in the menu part2

--- src/components/journal/JournalEntries.js
+++ src/components/journal/JournalEntries.js
@@ -6,7 +6,7 @@ import JournalEntry from './JournalEntry';
 
 export const JournalEntries = () => {
 
-    const entries = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
+    
     // the good think to use redux its we dont need to use props to bring the informations because
     // the state is changing in the user interface and we can take the values because they are in state redux memorie
     // i only i have to use use selector to bring everything
--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,28 +1,38 @@
-import React from 'react'
+import React from 'react';
+import moment from 'moment';
 
 export const JournalEntry = ({id, date, title, body, url }) => {
-    console.log(id, date, title, body, url)
+    //console.log(id, date, title, body, url)
+
+    const noteDate = moment(date);
+
     return (
         <div className='journal__entry pointer'>
-            <div className='journal__entry-picture'
-            style={{
-                backgroundSize:'cover',
-                backgroundImage:'url(https://www.landuum.com/wp-content/uploads/2019/03/cultura_paisajeiluminado_landuum5.jpg)'
-            }}
-            >
-            </div>
+
+
+            { 
+                //if url not undefined show this:
+                url &&
+                    <div className='journal__entry-picture'
+                        style={{
+                            backgroundSize:'cover',
+                            backgroundImage:`url(${ url } `
+                        }}
+                    >
+            </div>}
+
+            
             <div className='journal__entry-body'>
                 <p className='journal__entry-title'>
-                    Un nuevo dia
+                    {title}
                 </p>
                 <p className='journal__entry-content'>
-                Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-                make a type specimen book.
+                    {body}
                 </p>
             </div>
             <div className='journal__entry-date-box'>
-                <span>Monday</span>
-                <h4>28</h4>
+                <span>{noteDate.format('dddd')}</span>
+                    <h4>{noteDate.format('Do')}</h4>
             </div>
 
         </div>
