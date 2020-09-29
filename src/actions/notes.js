6f1ca86639cc48c48a80d1a3feb3109e80169120
David <jobroman83@gmail.com>
2020-09-29 16:23:22 +0200

    task 3.1 create a new note only with the date

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,9 +1,30 @@
+import { db } from "../firebase/firebase-config";
+
 
 export const startNewNote = () => {
-    return (dispatch, getState) => {
+    return async (dispatch, getState) => {
 
-        // to save in firestone        
-        const uid = getState()
-        console.log(uid)
+        // to save in firestone 
+        // from my entire state only i need: .auth.uid 
+        const uid = getState().auth.uid;
+        //console.log(uid)
+        const newNote ={ 
+            title:'',
+            body:'',
+            date: new Date().getTime()
+        }
+
+        const  docRef = await db.collection(`${uid}/journal/notes`).add( newNote);
+        console.log(docRef)
     }
 }
+
+
+// rules_version = '2';
+// service cloud.firestore {
+//   match /databases/{database}/documents {
+//     match /{document=**} {
+//       allow read, write: if true; <--- i change this to true in firestone
+//     }
+//   }
+// }
