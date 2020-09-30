06a02b2adf5a31aace9647e0fd88c4bbf0814d75
David <jobroman83@gmail.com>
2020-09-30 13:23:46 +0200

    task 7 active a note for the edition part1 putting in the state with id and info

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import moment from 'moment';
+import { activeNote } from '../../actions/notes';
+import {useDispatch} from 'react-redux';
 
 export const JournalEntry = ({id, date, title, body, url }) => {
     //console.log(id, date, title, body, url)
 
     const noteDate = moment(date);
+    const dispatch = useDispatch()
+
+    // to activate the note when you click 
+    const handleEntryClick = () => {
+        dispatch(activeNote(id,{
+                date,title,body,url
+            })
+        )
+    }
 
     return (
-        <div className='journal__entry pointer'>
+        <div className='journal__entry pointer'
+        onClick={handleEntryClick}
+        >
 
 
             { 
