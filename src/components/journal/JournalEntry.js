f25aad5629b1ecd8e1dc1164a480ad372cca8851
David <jobroman83@gmail.com>
2020-10-06 15:59:39 +0200

    Task 15 adding some animations

--- src/components/journal/JournalEntry.js
+++ src/components/journal/JournalEntry.js
@@ -16,9 +16,9 @@ export const JournalEntry = ({id, date, title, body, url }) => {
             })
         )
     }
-
+    
     return (
-        <div className='journal__entry pointer'
+        <div className='journal__entry pointer animate__animated animate__fadeIn animate__faster'
         onClick={handleEntryClick}
         >
 
